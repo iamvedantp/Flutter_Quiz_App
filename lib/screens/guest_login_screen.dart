afe5ec4938a3da04e04940fbfec34ea3f78e4e0d
iamvedantp <vedantp183@gmail.com>
2024-12-30 19:25:59 +0530

    Guest login page added

---
+++ lib/screens/guest_login_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class GuestLoginScreen extends StatefulWidget {
+  const GuestLoginScreen({super.key});
+
+  @override
+  State<GuestLoginScreen> createState() => _GuestLoginScreenState();
+}
+
+class _GuestLoginScreenState extends State<GuestLoginScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  void _navigateToHomeScreen() {
+    if (_nameController.text.trim().isNotEmpty) {
+      Navigator.pushNamed(context, '/home',
+          arguments: _nameController.text.trim());
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please enter your name!')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Guest Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Enter Your Name to Continue",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Your Name',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _navigateToHomeScreen,
+              child: const Text('Continue'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
